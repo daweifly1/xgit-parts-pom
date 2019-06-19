66415290eaffc218adcf110ad29f6587ddc1c62f
tigerwer@126.com <tigerwer@126.com>
2019-06-19 18:09:20 +0800

    misc

---
+++ gw-auth/gw-auth-api/src/main/java/cn/com/xgit/gw/http/consts/ZuulRequestHeader.java
@@ -0,0 +1,8 @@
+package cn.com.xgit.gw.http.consts;
+
+public class ZuulRequestHeader {
+    public static final String USER_ID = "x-user-id";
+    public static final String USER_NAME = "x-user-name";
+    public static final String REMOTE_IP = "x-remote-ip";
+
+}
